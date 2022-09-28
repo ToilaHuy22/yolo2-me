17f6b2b50d5f02de6d7d9a1a32a3156b21dc12f9
Huy <xinhtupeanh@gmail.com>
2022-09-28 21:53:25 +0700

    1 Config app and Create Header

---
+++ src/components/Layout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+
+import Routes from '../routes/Routes';
+
+const Layout = () => {
+	return (
+		<BrowserRouter>
+			<Route
+				render={(props) => (
+					<div>
+						<Header />
+						<div className="container">
+							<div className="main">
+								<Routes />
+							</div>
+						</div>
+					</div>
+				)}
+			/>
+		</BrowserRouter>
+	);
+};
+
+export default Layout;
