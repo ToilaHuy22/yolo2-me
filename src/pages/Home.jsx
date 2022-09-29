6f936bd43804f8cb516613ebb2bf5d207a5975b9
Huy <xinhtupeanh@gmail.com>
2022-09-29 22:05:20 +0700

    4 Create PolicyCard and Responsive, fix Responsive HeroSlider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,28 @@ import Section, { SectionBody, SectionTitle } from '../components/Section';
 import Grid from '../components/Grid';
 
 import heroSliderData from '../assets/fake-data/hero-slider';
+import policy from '../assets/fake-data/policy';
+import PolicyCard from '../components/PolicyCard';
 
 const Home = () => {
 	return (
 		<Helmet title="Home">
 			<HeroSlider data={heroSliderData} control={true} auto={true} timeOut={8000} />
+			<Section>
+				<SectionBody>
+					<Grid col={4} mdCol={2} smCol={1} gap={20}>
+						{policy.map((item, index) => (
+							<Link to="/policy" key={index}>
+								<PolicyCard
+									name={item.name}
+									description={item.description}
+									icon={item.icon}
+								/>
+							</Link>
+						))}
+					</Grid>
+				</SectionBody>
+			</Section>
 		</Helmet>
 	);
 };
